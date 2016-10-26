37a4117124861eee958b8a99c5fd510e28aa0871
Martin Bravo <bravomartin@gmail.com>
2016-10-26 16:58:16 -0300

    take out redux-throttle

--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk'
-import throttle from 'redux-throttle';
 import {Provider, connect} from 'react-redux'
 
 import reducer from './state'
 import AppLayout from './layout'
 
-const throttleMiddleware = throttle(3000, {trailing: true});
-
-const createStoreWithMiddleware = applyMiddleware(thunk, throttleMiddleware)(createStore);
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 const store = createStoreWithMiddleware(reducer);
 
 ReactDOM.render(
